e5ddce34defa13c8abbfde9d784ee6c49a44700d
ramirowang <ramirowang@tencent.com>
2022-11-16 11:30:39 +0800

    设置好开发环境

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import { __main__ } from "@/leet-code";
 
 const app = createApp(App);
 
 app.use(router);
 
 app.mount("#app");
+
+__main__();
